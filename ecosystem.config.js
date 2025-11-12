1678481396eff7b876b68803a2a1069c8c649ec5
Karula-k <d.fikriirsyad@gmail.com>
2025-11-12 14:25:34 +0700

    update fork instead cluester

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,6 +13,7 @@ module.exports = {
             out_file: 'log/out.log',
             node_args: '--tls-min-v1.0',
             instances: 1,
+            exec_mode: 'fork',
             autorestart: true,
             watch: false,
             max_memory_restart: '300M'
